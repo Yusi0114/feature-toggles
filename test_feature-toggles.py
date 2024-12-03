d92e248f380d39b780b3ae80075ce364e09fcb42
Yusi0114 <132541872+Yusi0114@users.noreply.github.com>
2024-12-03 16:25:01 +0800

    Update test_feature-toggles.py

--- test_feature-toggles.py
+++ test_feature-toggles.py
@@ -1,18 +1,9 @@
 import pygame
-import logging
-from featuretoggles import TogglesList
+from flagsmith import Flagsmith
+import os
 
-
-# 定義功能開關類
-class ReleaseToggles(TogglesList):
-    feature1: bool
-    feature2: bool
-    feature3: bool
-
-
-# 初始化功能開關
-toggles = ReleaseToggles('toggles.yaml')
-
+# 初始化Flagsmith SDK
+flagsmith = Flagsmith(environment_key=os.environ["FLAGSMITH_KEY"])
 
 # 初始化 Pygame
 pygame.init()
@@ -31,28 +22,29 @@ font = pygame.font.Font(None, 36)
 # 按鈕的矩形區域
 button_rect = pygame.Rect(250, 250, 300, 50)
 
-
 # 主函數
 def main():
     running = True
     while running:
         # 填充背景
         screen.fill(WHITE)
+        flags = flagsmith.get_environment_flags()
 
         # 處理事件
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
+
         # 畫按鈕（如果 feature1=True）
-        if toggles.feature1:
+        if flags.is_feature_enabled("feature1"):
             pygame.draw.rect(screen, GRAY, button_rect)  # 畫矩形按鈕
             button_text = font.render("FT1 is activated.", True, BLACK)
             screen.blit(button_text, button_rect.move(50, 10))  # 按鈕文字
 
         # 顯示文字（如果 feature2=True）
-        if toggles.feature2:
-            if toggles.feature3:
+        if flags.is_feature_enabled("feature2"):
+            if flags.is_feature_enabled("feature3"):
                 text_content = "FT2 is activated, and FT3 is, too."
             else:
                 text_content = "FT2 is activated, but FT3 is deactivated."
@@ -63,7 +55,6 @@ def main():
         pygame.display.flip()
 
     pygame.quit()
-
 
 if __name__ == "__main__":
     main()
